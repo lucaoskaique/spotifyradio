7a48d1de89e8c3151fae3dabece4c7f92cd49815
lucaoskaique <lucas.kaique175@hotmail.com>
2022-03-16 14:47:52 -0300

    init

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+const defaultConfig = {
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "text",
+    "lcov"
+  ],
+  coverageThre
+}
+export default {
+  projects: []
+};
